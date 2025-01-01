8aa54d92be19a9d96336ad8e5e0ae8d30d44269c
rfrian <rfrian111@gmail.com>
2025-01-02 01:12:24 +0600

    Login part

--- app/src/main/java/com/example/loginandsignup/LoginActivity.kt
+++ app/src/main/java/com/example/loginandsignup/LoginActivity.kt
@@ -2,25 +2,48 @@ package com.example.loginandsignup
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.loginandsignup.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private val binding : ActivityLoginBinding by lazy{
         ActivityLoginBinding.inflate(layoutInflater)
     }
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
         binding.signUpButton.setOnClickListener{
             val intent = Intent(this,SignupActivity::class.java)
             startActivity(intent)
             finish()
         }
-
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginUserGmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+            if(email.isEmpty() || password.isEmpty()){
+                binding.loginUserGmail.error = "Enter password and email"
+                binding.loginPassword.error = "Enter password and email"
+            }
+            else{
+                auth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            startActivity(Intent(this, HomeActivity::class.java))
+                            Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+            }
+        }
     }
 }
